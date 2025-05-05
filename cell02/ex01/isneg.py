6189b324e9ca2f86d564bc1c291c6aff284cddf1
Romain Lenarduzzi <rolenard@z2t12c4.42bangkok.com>
2025-05-05 18:58:02 +0700

    number equal or not to zero

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,7 @@
+enter_a_number = int(input())
+if enter_a_number > 0:
+    print("This number is positive.")
+elif enter_a_number < 0:
+    print("This number is negative.")
+else:
+    print("This number is both positive and negative.")
