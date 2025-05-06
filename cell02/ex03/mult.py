f15e19a74fc0f023efde1a957dc89010e91ad911
Romain Lenarduzzi <rolenard@z1t5c2.42bangkok.com>
2025-05-06 13:16:24 +0700

    multiply

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
-first_number = int(input("Enter the first number:"))
-second_number = int(input("Enter the second number:"))
-print(first_number * second_number)
-print(f"{first_number} * {second_number} = {first_number * second_number}")
+first_number = int(input("Enter the first number:\n"))
+second_number = int(input("Enter the second number:\n"))
+result = first_number * second_number
+print(result)
+print(f"{first_number} * {second_number} = {result}")
 
 def multiply(first_number, second_number):
-    if first_number * second_number > 0:
+    if result > 0:
         return"The result is positive."
-    elif first_number * second_number < 0:
+    elif result < 0:
         return "The result is negative."
     else:
         return "The result is positive and negative."
