9c034fc60b42c69f804220a136f3bf23d29716b7
Romain Lenarduzzi <rolenard@z1t5c2.42bangkok.com>
2025-05-06 13:27:09 +0700

    multiply

---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+my_number = int(input("Enter a number").strip())
+counter = 1
+
+while counter <= 10:
+    table = my_number * counter
+    print{f"{my_number * counter}"}
+    counter += 1
+
