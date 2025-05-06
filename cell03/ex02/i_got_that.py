017d2d42c3cea1f50e4e5c1ce3edde55c48d4311
Romain Lenarduzzi <rolenard@z1t5c2.42bangkok.com>
2025-05-06 16:02:03 +0700

    STOP break

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-input("What you gotta say? :")
+a = input("What you gotta say? :")
 
-while True:
-    answer = input("I got that! Anything else? :")
-    if answer == "STOP":
-        break
+while a != "STOP":
+    a = input("I got that! Anything else? :")
