fecf7daa453ebe6fcc1cf66a03d3e4629cf962d2
Romain Lenarduzzi <rolenard@z1t5c2.42bangkok.com>
2025-05-06 15:10:02 +0700

    multiplication

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -4,7 +4,7 @@ first_number = int(input("Enter the first number:\n"))
 second_number = int(input("Enter the second number:\n"))
 result = first_number * second_number
 print(result)
-print(f"{first_number} * {second_number} = {result}")
+print(f"{first_number} x {second_number} = {result}")
 
 if result > 0:
     print("The result is positive.")
