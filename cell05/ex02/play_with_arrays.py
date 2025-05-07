f233fb94a9273e190d86544a88bc8b7b11b77e91
Romain Lenarduzzi <rolenard@z2t12c4.42bangkok.com>
2025-05-07 14:31:52 +0700

    modify array

---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+numbers = [2, 8 ,9, 48 ,8, 22, -12, 2]
+print(numbers)
+filtered_numbers = list(filter(lambda x : x > 5, numbers))
+
+for i in range(len(filtered_numbers)):
+        filtered_numbers[i] += 2
+print(filtered_numbers)
