dcff02f3f0009d05da909b8080013effea9e76f4
Romain Lenarduzzi <rolenard@z2t12c4.42bangkok.com>
2025-05-07 10:57:44 +0700

    Calculation

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+first_number = int(input("Give me the first number: "))
+seconde_number = int(input("Give me the second number: "))
+
+print("Thank you!")
+
+print(f"{first_number} + {seconde_number} = {first_number + seconde_number}")
+print(f"{first_number} - {seconde_number} = {first_number - seconde_number}")
+print(f"{first_number} / {seconde_number} = {first_number / seconde_number}")
+print(f"{first_number} * {seconde_number} = {first_number * seconde_number}")
